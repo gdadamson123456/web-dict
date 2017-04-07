60da511059eb91f6cd304aa7d534aac5b4719a6b
Alexander Kostenko <sanyokkua@gmail.com>
2017-04-07 18:55:17 +0300

    Added test role and user.

--- src/main/webapp/angular/app/services/in-memory-data.service.ts
+++ src/main/webapp/angular/app/services/in-memory-data.service.ts
@@ -3,11 +3,13 @@ export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         let roles = [
             {id: 1, name: 'admin'},
-            {id: 2, name: 'user'}
+            {id: 2, name: 'user'},
+            {id: 3, name: 'kaka'}
         ];
         let users = [
             {id: 1, login: 'admin', password: 'welcome', email: 'admin@gmail.com', role: {id: 1, name: 'admin'}},
-            {id: 2, login: 'user', password: 'user_welcome', email: 'user@gmail.com', role: {id: 2, name: 'user'}}
+            {id: 2, login: 'user', password: 'user_welcome', email: 'user@gmail.com', role: {id: 2, name: 'user'}},
+            {id: 3, login: 'ukaka', password: 'user_welcome', email: 'user@gmail.com', role: {id: 3, name: 'kaka'}}
         ];
         return {roles: roles, users: users};
     }
