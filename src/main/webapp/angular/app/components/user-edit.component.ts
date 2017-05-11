aeda3aad4411244f56ac07eebc922c68926dc7a0
Alexander Kostenko <sanyokkua@gmail.com>
2017-05-11 19:58:04 +0300

    Code cleanup. Removed custom validator.

--- src/main/webapp/angular/app/components/user-edit.component.ts
+++ src/main/webapp/angular/app/components/user-edit.component.ts
@@ -29,6 +29,7 @@ export class UserEditComponent extends AbstractEditComponent implements OnInit {
     }
 
     onRoleSelect(value: string): void {
+
         this.roleService.getRoles().then(roles => {
             let selectedRole = roles.find(r => r.name == value);
             this.user.role = selectedRole;
